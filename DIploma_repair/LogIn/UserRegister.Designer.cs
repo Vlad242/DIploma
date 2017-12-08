894b9144cbe77ad2b2ab0270e4669be8918e96e9
vlad242 <vlad242@i.ua>
2017-12-08 02:21:34 +0200

    fix close register bug

--- DIploma_repair/LogIn/UserRegister.Designer.cs
+++ DIploma_repair/LogIn/UserRegister.Designer.cs
@@ -455,7 +455,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(154, 189);
             this.button1.TabIndex = 19;
-            this.button1.Text = "ADD";
+            this.button1.Text = "Register";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
